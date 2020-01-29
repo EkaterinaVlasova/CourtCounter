0c48448d7562a55d2d797bc2e9e9b5596a89b04c
tgohil <trishulgohil@live.co.uk>
2020-01-29 17:32:58 +0000

    Convert first test to use robot pattern

--- app/src/androidTest/java/com/example/android/courtcounter/tests/CourtCounterTest.kt
+++ app/src/androidTest/java/com/example/android/courtcounter/tests/CourtCounterTest.kt
@@ -1,15 +1,21 @@
 package com.example.android.courtcounter.tests
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.courtcounter.MainActivity
 import com.example.android.courtcounter.R
 import com.example.android.courtcounter.utils.BaseTest
+import com.example.android.courtcounter.robot.CounterTestRobot
+import org.hamcrest.Matchers
 import org.hamcrest.Matchers.allOf
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -17,16 +23,26 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class MyFirstTest: BaseTest() {
 
-    val TEAM_A_SCORE = "0"
+    //This test class will be testing the interaction between the HomeActivity and DetailsActivity
+    private lateinit var counterTestRobot: CounterTestRobot
+
+
+    @Before
+    fun setup() {
+        counterTestRobot = CounterTestRobot()
+    }
+
 
     @Test
     fun checkTeamAPoints() {
 
-        ActivityScenario.launch(MainActivity::class.java)
+        with(counterTestRobot){
+            launchApp()
+        }
 
-        onView(allOf(withId(R.id.team_a_score), withText(TEAM_A_SCORE)))
-                .check(matches(isCompletelyDisplayed()))
-
+        with(counterTestRobot) {
+            checkTeamAPointsIsDisplayed()
+        }
     }
 
     @Test
